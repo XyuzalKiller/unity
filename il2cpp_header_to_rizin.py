4f6a7458c9442fea9a0784268dc29cde74b84b39
XyuzalKiller <189678824+XyuzalKiller@users.noreply.github.com>
2025-12-31 21:43:47 -0200

    Add files via upload

---
+++ il2cpp_header_to_rizin.py
@@ -0,0 +1,34 @@
+import re
+
+if __name__ == "__main__":
+    inheritance_pattern = re.compile(r": (\w+) {")
+
+    unnamed_union_pattern = re.compile(r"union(?=\s*{)")
+    unnamed_union_count = 0
+
+    def name_unnamed_union(m):
+        nonlocal unnamed_union_count
+        unnamed_union_count += 1
+        return f"union u{unnamed_union_count}"
+
+    with open("il2cpp.h", "r") as f:
+        data = f.read()
+
+        data = data.replace(
+            "typedef void(*Il2CppMethodPointer)();",
+            "typedef void(*Il2CppMethodPointer)(void);",
+            1
+        )
+
+        data = unnamed_union_pattern.sub(
+            name_unnamed_union,
+            data
+        )
+
+        data = inheritance_pattern.sub(
+            r"{\n \1 super;",
+            data
+        )
+
+    with open("il2cpp_rizin.h", "w") as f:
+        f.write(data)
