782af64dded1bd1b315948028edf428d6b39a1ec
XyuzalKiller <189678824+XyuzalKiller@users.noreply.github.com>
2025-12-31 16:29:23 -0800

    Update il2cpp_header_to_rizin.py

--- il2cpp_header_to_rizin.py
+++ il2cpp_header_to_rizin.py
@@ -21,11 +21,6 @@ def main():
             1
         )
 
-        data = data.replace(
-            "Il2CppRGCTXData*",
-            "void*"
-        )
-
         data = inheritance_pattern.sub(
             r"{\n\1 super;",
             data
