ff4710fd696106a136ffc51a3e69dc2bfc311a04
XyuzalKiller <189678824+XyuzalKiller@users.noreply.github.com>
2026-01-01 06:47:05 +0400

    Update il2cpp_header_to_rizin.py

--- il2cpp_header_to_rizin.py
+++ il2cpp_header_to_rizin.py
@@ -1,10 +1,10 @@
 import re
 
 def main():
-    il2cpp_rgctx_data_pattern = re.compile(r"union Il2CppRGCTXData\s*\{.*?\};", re.DOTALL)
+    il2cpp_rgctx_data_pattern = re.compile(r"union Il2CppRGCTXData\n\{.*?\}", re.DOTALL)
     inheritance_pattern = re.compile(r": (\w+) {")
 
-    unnamed_union_pattern = re.compile(r"union\s*\{.*?\}(?=;)", re.DOTALL)
+    unnamed_union_pattern = re.compile(r"union\n\{.*?\}(?=;)", re.DOTALL)
     unnamed_union_count = 0
 
     def name_unnamed_union(m):
@@ -23,7 +23,7 @@ def main():
         )
 
         data = il2cpp_rgctx_data_pattern.sub(
-            "typedef void* Il2CppRGCTXData;",
+            "typedef void* Il2CppRGCTXData",
             data,
             count=1
         )
