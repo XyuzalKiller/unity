67d2c8805b7da011c954e4ed83e5e3df76a66db7
XyuzalKiller <189678824+XyuzalKiller@users.noreply.github.com>
2026-01-01 10:55:31 +0200

    Update il2cpp_header_to_rizin.py

--- il2cpp_header_to_rizin.py
+++ il2cpp_header_to_rizin.py
@@ -19,24 +19,6 @@ def main():
             1
         )
 
-        data = data.replace(
-            "Il2CppRGCTXData* rgctx_data;",
-            "void* rgctx_data;"
-        )
-
-        data = re.sub(
-            r"union\s*{\s*const void\* rgctx_data;\s*const void\* methodMetadataHandle;\s*}",
-            "const void* rgctx_data",
-            data,
-            1
-        )
-
-        data = re.sub(
-            r"union\s*{\s*const void\* genericMethod;\s*const void\* genericContainerHandle;\s*}",
-            "const void* genericMethod",
-            data,
-            1
-        )
 
         data = re.sub(
             r": (\w+) {",
